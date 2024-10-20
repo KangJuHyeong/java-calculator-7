a3dacd2c242245c4c4af28f9588665e22ce7b51b
KangJuHyeong <hjk7935@naver.com>
2024-10-20 23:53:52 +0900

    feat: 문자열 입력 받기, 결과 출력 기능 구현

---
+++ src/main/java/view/printOutput.java
@@ -0,0 +1,7 @@
+package view;
+
+public class printOutput {
+    public static void printOutput(int ret){
+        System.out.println("결과:"+ret);
+    }
+}
---
+++ src/main/java/view/readInput.java
@@ -0,0 +1,10 @@
+package view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class readInput {
+    public static String readInput(){
+        System.out.println("덧셈할 문자열을 입력해주세요:");
+        return Console.readLine();
+    }
+}
